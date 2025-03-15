4f38876b930e27c24927c8f6c233c29c09026d75
TrinhHuuGiaBao872k4 <giabao872004@gmail.com>
2025-03-16 00:08:57 +0700

    second commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,41 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WebSanCauLong.Models;
 
 namespace WebSanCauLong.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly DataModel _dataModel = new DataModel();
+        public ActionResult Index(int? page)
         {
-            return View();
+            string query = "SELECT TOP 4 * FROM San WHERE TrangThai = 'Còn trống' ORDER BY NEWID()";
+            ArrayList danhSachSan = _dataModel.get(query);
+            return View(danhSachSan);
+        }
+        // Chi tiết sân
+        public ActionResult ChiTietSan(int id)
+        {
+            string query = $"SELECT * FROM San WHERE SanID = {id}";
+            ArrayList san = _dataModel.get(query);
+            if (san.Count == 0)
+            {
+                return HttpNotFound();
+            }
+            return View(san[0]);
         }
 
-        public ActionResult About()
+        // Tìm kiếm sân
+        public ActionResult TimKiem(string keyword)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            string query = $"SELECT * FROM San WHERE TenSan LIKE '%{keyword}%' OR DiaChi LIKE '%{keyword}%'";
+            ArrayList ketQua = _dataModel.get(query);
+            return View("Index", ketQua);
         }
     }
 }
