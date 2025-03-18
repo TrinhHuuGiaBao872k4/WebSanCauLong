04478538e9211dc031634585e27f65e77c5004d8
TrinhHuuGiaBao872k4 <giabao872004@gmail.com>
2025-03-19 01:08:00 +0700

    four commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSanCauLong.Models;
+using WebSanCauLong.Models.ViewModels;
+
+namespace WebSanCauLong.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly DataModel _dataModel = new DataModel();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
